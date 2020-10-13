79dbb98995685c1cc115e8c59dcafd1003a8dd14
SVSalenko <S.V.Salenko@gmail.com>
2020-10-13 18:40:16 +0300

    Homework 1. Variables, operators, data types.

---
+++ 1/assignment.js
@@ -0,0 +1,13 @@
+let userEnteredNumber = 12;
+let result;
+
+result = 18 + userEnteredNumber;
+
+result = result - 10;
+result = result * 2;
+result = result / 5;
+
+// Значение переменно введенной поьзователем не изменилось.
+
+alert('Результат - ' + result);
+alert('Число, введённое пользователем - ' + userEnteredNumber);
