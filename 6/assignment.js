1a5e0afae9ce356cfe6cafdaff101eac927d7d36
SVSalenko <S.V.Salenko@gmail.com>
2020-11-28 23:34:56 +0300

    Homework 6. DOM queries and manipulations.

---
+++ 6/assignment.js
@@ -0,0 +1,12 @@
+// Task №1
+const taskWayOne = document.getElementById('task-1');
+const taskWayTwo = document.querySelector('#task-1');
+taskWayOne.style.backgroundColor = 'black';
+taskWayTwo.classList.add('tex-color');
+
+// Task №2
+document.querySelector('head title').text = 'Домашнее задание сделано!';
+document.head.querySelector('title').text = 'Домашнее задание сделано!!!';
+
+// Task №3
+document.querySelector('h1').innerText = 'Домашнее задание сделано!';
